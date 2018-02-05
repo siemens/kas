cda778acb57e12f9df7ec1b53150c805a9aeda73
Jan Kiszka <jan.kiszka@siemens.com>
2018-02-05 20:36:13 +0100

    libcmds: Write MACHINE and DISTRO as weak defaults
    Otherwise we cannot overwrite them in mulitconfig files. It's not
    expected that anything else in the layer config files sets those
    variable, thus shadows our setting in local.conf.
    
    Something changed in latest bitbake to reveal this issue. We first see
    it in Isar since bitbake moved to f8126aaf7741.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kas/libcmds.py
+++ kas/libcmds.py
@@ -183,8 +183,8 @@ class WriteConfig(Command):
             filename = config.build_dir + '/conf/local.conf'
             with open(filename, 'w') as fds:
                 fds.write(config.get_local_conf_header())
-                fds.write('MACHINE ?= "{}"\n'.format(config.get_machine()))
-                fds.write('DISTRO ?= "{}"\n'.format(config.get_distro()))
+                fds.write('MACHINE ??= "{}"\n'.format(config.get_machine()))
+                fds.write('DISTRO ??= "{}"\n'.format(config.get_distro()))
                 fds.write('BBMULTICONFIG ?= "{}"\n'.format(
                     config.get_multiconfig()))
 
