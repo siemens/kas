e24f8cff7c72faafd85dbf82b2adf8caddbe7fe9
Daniel Wagner <daniel.wagner@siemens.com>
2018-12-07 09:41:20 +0100

    scripts: Add flake8 checker
    Add code and style checker flake8.
    
    Signed-off-by: Daniel Wagner <daniel.wagner@siemens.com>

--- scripts/checkcode.sh
+++ scripts/checkcode.sh
@@ -5,6 +5,9 @@ ERROR=0
 echo "Checking with pycodestyle"
 pycodestyle --ignore=W503,W606 $1/*.py $1/*/*.py || ERROR=$(expr $ERROR + 1)
 
+echo "Checking with flake8"
+flake8 $1 || ERROR=$(expr $ERROR + 2)
+
 echo "Checking with doc8"
 doc8  $1/docs --ignore-path $1/docs/_build || ERROR=$(expr $ERROR + 4)
 
