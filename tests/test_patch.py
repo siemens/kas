fafc017392f9e8f8ab9fe69173cc17b3403c6046
Henning Schild <henning.schild@siemens.com>
2019-08-20 16:58:59 +0200

    tests: add a testcase for applying patches
    This adds a test where we clone a git and a hg repo and apply a single
    patch and later a quilt series on each of them.
    
    Signed-off-by: Henning Schild <henning.schild@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ tests/test_patch.py
@@ -0,0 +1,35 @@
+# kas - setup tool for bitbake based projects
+#
+# Copyright (c) Siemens AG, 2019
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import shutil
+from kas import kas
+
+import pytest  # noqa: F401; flake8 'pytest' imported but unused
+
+
+def test_patch(tmpdir):
+    tdir = str(tmpdir.mkdir('test_patch'))
+    shutil.rmtree(tdir, ignore_errors=True)
+    shutil.copytree('tests/test_patch', tdir)
+    os.chdir(tdir)
+    kas.kas(['shell', 'test.yml', '-c', 'true'])
