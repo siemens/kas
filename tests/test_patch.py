e29035407f1998e1e448dc22fc538c1eae3a1e1f
Henning Schild <henning.schild@siemens.com>
2019-09-24 09:18:25 +0200

    tests: test patching of symbolic refspecs i.e branches
    That once caused issues when rolling back (checkout) since our local
    branch we patched and commited on had the desired name.
    
    Signed-off-by: Henning Schild <henning.schild@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -36,3 +36,4 @@ def test_patch(tmpdir):
     kas.kas(['shell', 'test.yml', '-c', 'true'])
     for f in ['kas/tests/test_patch/hello.sh', 'hello/hello.sh']:
         assert os.stat(f)[stat.ST_MODE] & stat.S_IXUSR
+    kas.kas(['shell', 'test.yml', '-c', 'true'])
