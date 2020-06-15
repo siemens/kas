c2bbfee8f3d1f72fdd2b5fdae4f5c9d2b3b4e5a9
Paul Barker <pbarker@konsulko.com>
2020-06-15 22:03:32 +0200

    tests: Check refspec switch updates git branch/commit correctly
    Mercurial has no concept of a local tracking branch so we only need to
    test git repositories here.
    
    Signed-off-by: Paul Barker <pbarker@konsulko.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ tests/test_refspec.py
@@ -0,0 +1,71 @@
+# kas - setup tool for bitbake based projects
+#
+# Copyright (c) Siemens AG, 2019
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import shutil
+import subprocess
+from kas import kas
+from kas.libkas import run_cmd
+
+import pytest  # noqa: F401; flake8 'pytest' imported but unused
+
+
+def test_refspec_switch(tmpdir):
+    """
+        Test that the local git clone is correctly updated when switching
+        between a commit hash refspec and a branch refspec.
+    """
+    tdir = str(tmpdir.mkdir('test_refspec_switch'))
+    shutil.rmtree(tdir, ignore_errors=True)
+    shutil.copytree('tests/test_refspec', tdir)
+    prev_dir = os.path.realpath(os.getcwd())
+    os.chdir(tdir)
+
+    kas.kas(['shell', 'test.yml', '-c', 'true'])
+    (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas',
+                           fail=False, liveupdate=False)
+    assert rc != 0
+    assert output.strip() == ''
+    (rc, output) = run_cmd(['git', 'rev-parse', '-q', 'HEAD'], cwd='kas',
+                           fail=False, liveupdate=False)
+    assert rc == 0
+    assert output.strip() == '907816a5c4094b59a36aec12226e71c461c05b77'
+    (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas2',
+                           fail=False, liveupdate=False)
+    assert rc == 0
+    assert output.strip() == 'refs/heads/master'
+
+    kas.kas(['shell', 'test2.yml', '-c', 'true'])
+    (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas',
+                           fail=False, liveupdate=False)
+    assert rc == 0
+    assert output.strip() == 'refs/heads/master'
+    (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas2',
+                           fail=False, liveupdate=False)
+    assert rc != 0
+    assert output.strip() == ''
+    (rc, output) = run_cmd(['git', 'rev-parse', '-q', 'HEAD'], cwd='kas2',
+                           fail=False, liveupdate=False)
+    assert rc == 0
+    assert output.strip() == '907816a5c4094b59a36aec12226e71c461c05b77'
+
+    os.chdir(prev_dir)
