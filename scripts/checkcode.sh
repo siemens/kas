6f8d5d6c781f46460cf1a6b6ae2d6d08d07e8bec
Jan Kiszka <jan.kiszka@siemens.com>
2021-01-29 20:58:11 +0100

    checkcode: Address shellcheck warnings
    ...and stop ignoring them all.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- scripts/checkcode.sh
+++ scripts/checkcode.sh
@@ -8,15 +8,15 @@ if [ $# != 1 ]; then
 fi
 
 echo "Checking with pycodestyle"
-pycodestyle --ignore=W503,W606 $1/*.py $1/*/*.py || ERROR=$(expr $ERROR + 1)
+pycodestyle --ignore=W503,W606 "$1"/*.py "$1"/*/*.py || ERROR=$((ERROR + 1))
 
 echo "Checking with flake8"
-flake8 $1 || ERROR=$(expr $ERROR + 2)
+flake8 "$1" || ERROR=$((ERROR + 2))
 
 echo "Checking with doc8"
-doc8  $1/docs --ignore-path $1/docs/_build || ERROR=$(expr $ERROR + 4)
+doc8  "$1"/docs --ignore-path "$1"/docs/_build || ERROR=$((ERROR + 4))
 
 echo "Checking with shellcheck"
-shellcheck -S warning $1/kas-container $1/scripts/release.sh $1/scripts/checkcode.sh || ERROR=$(expr $ERROR + 8)
+shellcheck "$1"/kas-container "$1"/scripts/release.sh "$1"/scripts/checkcode.sh || ERROR=$((ERROR + 8))
 
 exit $ERROR
