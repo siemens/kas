148bbb2d80ae7325c9e6fdaeda88e8d81235e40b
Jan Kiszka <jan.kiszka@siemens.com>
2023-10-06 09:29:58 +0200

    docs: Fix readthedocs build
    Our theme must be listed under extensions and in the requirements.txt
    now.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinxarg.ext',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
