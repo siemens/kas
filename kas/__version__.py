af82e383a0346dfb31171fc689cb9ca7ffe14d64
Félix Piédallu <felix@piedallu.me>
2023-10-18 16:34:18 +0200

    Bump format version to 15 with the new `tags` key in `repos`, update tests formats
    Signed-off-by: Félix Piédallu <felix@piedallu.me>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- kas/__version__.py
+++ kas/__version__.py
@@ -28,5 +28,5 @@ __copyright__ = 'Copyright (c) Siemens AG, 2017-2020'
 __version__ = '4.0'
 
 # Please update docs/format-changelog.rst when changing the file version.
-__file_version__ = 14
+__file_version__ = 15
 __compatible_file_version__ = 1
