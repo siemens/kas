siemens/kas

commit af6b9ae3b368a6a3181367f285bd78de29920b04

auto-inject git credentials on gitlab ci
When running in the gitlab ci, we now automatically configure git to
clone the repositories on the CI server using https and the
CI_JOB_TOKEN. For backwards compatibility, this auto-injection is only
performed if no local git config or SSH config is provided.
This makes it easier for our users to use KAS in the Gitlab CI, as
things just work out of the box without any manual configuration needed.

For the Github CI this was already possible. By that, no change is
needed.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Tested-by: Frieder Schrempf <frieder.schrempf@kontron.de>
Reviewed-by: Frieder Schrempf <frieder.schrempf@kontron.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/libcmds.py
@@ -192,6 +192,23 @@ class SetupHome(Command):
             return 'GitLab CI'
         return None
 
+    @staticmethod
+    def _ssh_config_present():
+        """
+            Checks if any file in the .ssh dir exists or
+            any manual ssh config option is set.
+        """
+        ssh_vars = ['SSH_PRIVATE_KEY', 'SSH_PRIVATE_KEY_FILE', 'SSH_AUTH_SOCK']
+        if any(e in os.environ for e in ssh_vars):
+            return True
+
+        ssh_path = os.path.expanduser('~/.ssh')
+        if os.path.isdir(ssh_path):
+            with os.scandir(ssh_path) as it:
+                if any(it):
+                    return True
+        return False
+
     def _setup_netrc(self):
         if os.environ.get('NETRC_FILE', False):
             shutil.copy(os.environ['NETRC_FILE'],
@@ -254,6 +271,17 @@ class SetupHome(Command):
                 if os.environ.get('GIT_CREDENTIAL_USEHTTPPATH', False):
                     config['credential']['useHttpPath'] = \
                         os.environ.get('GIT_CREDENTIAL_USEHTTPPATH')
+            # in GitLab CI, add ssh -> https rewrites if no config is present
+            ci_server = os.environ.get('CI_SERVER_HOST', False)
+            if self._on_ci() == 'GitLab CI' and ci_server and \
+                    not self._ssh_config_present() and \
+                    not os.path.exists(gitconfig_host):
+                logging.debug('Adding GitLab CI ssh -> https rewrites')
+                section = f'url "https://{ci_server}/"'
+                config.add_value(section, 'insteadOf',
+                                 f'git@{ci_server}:')
+                config.add_value(section, 'insteadOf',
+                                 f'ssh://git@{ci_server}/')
             config.write()
 
     def execute(self, ctx):

commit 66b7d795d64b3aefd891023043e9b26d68f3807f

GitLab CI: make ssh config check less strict
For backwards compatibility we only inject the ssh rewrites if no ssh
config is present. The documentation was a bit imprecise w.r.t. what
exactly ssh config means, but the implementation checked for any file in
~/.ssh. This is too strict, as the authorized_keys or known_hosts files
are sometimes used to inject the ssh fingerprint of the own GitLab
instance. The presence of these files does not interfere with our
rewrite rules.

We change that by only checking for kas related ssh config vars as well
as the presence of `~/.ssh/config`.

Reported-by: Florian Bezdeka <florian.bezdeka@siemens.com>
Fixes: af6b9ae3 ("auto-inject git credentials on gitlab ci")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Reviewed-by: Frieder Schrempf <frieder.schrempf@kontron.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/libcmds.py
@@ -193,18 +193,16 @@ class SetupHome(Command):
     @staticmethod
     def _ssh_config_present():
         """
-            Checks if any file in the .ssh dir exists or
-            any manual ssh config option is set.
+            Checks if the .ssh/config file exists or any manual ssh config
+            option is set.
         """
         ssh_vars = ['SSH_PRIVATE_KEY', 'SSH_PRIVATE_KEY_FILE', 'SSH_AUTH_SOCK']
         if any(e in os.environ for e in ssh_vars):
             return True
 
         ssh_path = os.path.expanduser('~/.ssh')
-        if os.path.isdir(ssh_path):
-            with os.scandir(ssh_path) as it:
-                if any(it):
-                    return True
+        if os.path.exists(os.path.join(ssh_path, 'config')):
+            return True
         return False
 
     def _setup_netrc(self):