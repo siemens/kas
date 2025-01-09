siemens/kas

commit 0694dd670c5adeb85859409eae12cc4ce4e94ed4

fix(GitRepo): use git diff-index to check if dirty
git status -s fails in VSCode devcontainers, when users have
status.branch = true set in their global .gitconfig, as the branch name
is always printed and the dirty-check checks for any stdout output.

This would not be a problem, if we could check the return code instead,
but hg status will exit with zero. Otherwise, we could add the --quiet
option to the git is_dirty_cmd.

[Felix]
This change introduces a slight behavior change w.r.t. what is
considered "dirty". Previously, also untracked files of a git repo made
it dirty, while now only changed tracked files make it dirty. The new
behavior better reflects the git semantics of "dirty". This also
requires adapting a testcase. This also reverts the behavior change
introduced in 7b6cb815f1 ("fix(hg): align semantics of repo dirty
checking").

Signed-off-by: Oliver Kästner <okaestner@rosen-nxt.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/repos.py
@@ -620,7 +620,7 @@ class GitRepo(RepoImpl):
         return cmd
 
     def is_dirty_cmd(self):
-        return ['git', 'status', '-s']
+        return ['git', 'diff-index', 'HEAD', '--']
 
     def resolve_branch_cmd(self):
         refspec = self.remove_ref_prefix(self.branch or self.refspec)
@@ -697,7 +697,8 @@ class MercurialRepo(RepoImpl):
         return ['hg', 'pull']
 
     def is_dirty_cmd(self):
-        return ['hg', 'status']
+        return ['hg', 'status', '--modified', '--added',
+                '--removed', '--deleted']
 
     def resolve_branch_cmd(self):
         if self.branch:

tests/test_patch.py
@@ -127,7 +127,7 @@ def test_patch_dirty_repo(monkeykas, tmpdir, mercurial):
         ['sed', '-i', f's/82e55d328c8c/{commit}/', 'test.yml'])
 
     kas.kas(['checkout', '--skip', 'repos_apply_patches', 'test.yml'])
-    with open('kas-branch/make-dirty', 'a') as f:
+    with open('kas-branch/README.rst', 'a') as f:
         f.write('echo "dirty"')
     kas.kas(['checkout', 'test.yml'])
     # the dirty repo must not be patched

commit 00a0ffaac7d29bd5e17cbf5e15146a0a3defa5d3

fix(GitRepo): use git diff --stat to check if dirty
This check previously was changed to use git diff-index to check if a
working tree contains tracked-but-changed files. However, the diff-index
command only compares the results against the git index but does not
re-scan the filesystem (the index is eventually consistent). By that,
the check incorrectly reported a repo as dirty if the index was not
updated yet.

We now switch to the high-level command "git diff" which takes care of
updating the index beforehand. This command also is not affected by the
"status.branch = true" config option, which was the reason we switched
to git diff-index.

Fixes: 0694dd670c5 ("fix(GitRepo): use git diff-index to check if dirty")

Reported-by: Jan Kiszka <jan.kiszka@siemens.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/repos.py
@@ -633,7 +633,7 @@ class GitRepo(RepoImpl):
         return cmd
 
     def is_dirty_cmd(self):
-        return ['git', 'diff-index', 'HEAD', '--']
+        return ['git', 'diff', '--stat']
 
     def resolve_branch_cmd(self):
         refspec = self.remove_ref_prefix(self.branch or self.refspec)