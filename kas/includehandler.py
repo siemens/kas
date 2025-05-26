siemens/kas

commit a4792e23ebd76756238e425cd7266ece7b11a590

schema: make validation error message less verbose
In case the config does not validate against the schema a very long
error message was printed which even included the schema definition
itself (per single error). This has proven to be not very helpful,
as it made the actual error hard to spot.

We change this by following the upstream recommendation of sorting the
errors and also only printing the error message (which only contains the
affected node), reducing the error message from >300 to a couple
of lines. When running in debug mode, we explicitly dump the schema on
error, but only once for all errors.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/includehandler.py
@@ -75,11 +75,13 @@ def load_config(filename):
     validator = validator_class(CONFIGSCHEMA)
     validation_error = False
 
-    for error in validator.iter_errors(config):
+    for error in sorted(validator.iter_errors(config), key=str):
         validation_error = True
-        logging.error('Config file validation Error:\n%s', error)
+        logging.error('Config file validation Error:\n%s', error.message)
 
     if validation_error:
+        logging.debug('Validation against this schema failed:\n%s',
+                      json.dumps(error.schema, indent=2))
         raise LoadConfigException('Error(s) occured while validating the '
                                   'config file', filename)
 

commit 117e4d978b9ad3e958bbd7a6343a769396d5e627

includehandler: Dump schema per error
The validator may only pass a subschema via error. So, the schema of
last error may not be the one of the first error.

Fixes: a4792e23ebd7 ("schema: make validation error message less verbose")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/includehandler.py
@@ -108,10 +108,10 @@ class ConfigFile():
             logging.error('Config file validation Error:\n%s', error.message)
             logging.error('For a list of supported configuration elements, '
                           'see %s', PROJECT_CONFIG_URL)
-
-        if validation_error:
             logging.debug('Validation against this schema failed:\n%s',
                           json.dumps(error.schema, indent=2))
+
+        if validation_error:
             raise LoadConfigException('Error(s) occured while validating the '
                                       'config file', filename)
 