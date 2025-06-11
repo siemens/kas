siemens/kas

commit a2480fe59b6421eb96cf3bd86527ae6e412a331e

add support to verify signatures of git repos
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/libcmds.py
@@ -31,6 +31,7 @@ import pprint
 import configparser
 import json
 import base64
+from pathlib import Path
 from git.config import GitConfigParser
 from .libkas import (ssh_cleanup_agent, ssh_setup_agent, ssh_no_host_key_check,
                      get_build_environ, repos_fetch, repos_apply_patches)
@@ -38,6 +39,8 @@ from .context import ManagedEnvironment as ME
 from .context import get_context
 from .includehandler import IncludeException
 from .kasusererror import ArgsCombinationError
+from .keyhandler import GPGKeyHandler, SSHKeyHandler
+from .repos import RepoRefError
 
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) Siemens AG, 2017-2018'
@@ -78,6 +81,7 @@ class Macro:
                 repo_loop,
                 FinishSetupRepos(),
                 ReposCheckout(),
+                ReposCheckSignatures(),
                 ReposApplyPatches(),
                 SetupEnviron(),
                 WriteBBConfig(),
@@ -577,3 +581,55 @@ class ReposCheckout(Command):
     def execute(self, ctx):
         for repo in ctx.config.get_repos():
             repo.checkout()
+
+
+class ReposCheckSignatures(Command):
+    """
+        Imports the keys defined in the configuration and checks the
+        signatures of the repositories.
+    """
+
+    def __str__(self):
+        return 'repos_check_signatures'
+
+    def execute(self, ctx):
+        self._import_keys(ctx)
+        self._check_signatures(ctx)
+
+    def _import_keys(self, ctx):
+        handlerdirs = {
+            'gpg': Path(ctx.kas_work_dir) / '.gnupg',
+            'ssh': Path(ctx.kas_work_dir) / '.ssh-handler',
+        }
+        for _, d in handlerdirs.items():
+            d.mkdir(exist_ok=True)
+            d.chmod(0o700)
+
+        ctx.keyhandler['gpg'] = \
+            GPGKeyHandler(handlerdirs['gpg'],
+                          ctx.config.get_signers_config('gpg'),
+                          ctx.config)
+        ctx.keyhandler['ssh'] = \
+            SSHKeyHandler(handlerdirs['ssh'],
+                          ctx.config.get_signers_config('ssh'),
+                          ctx.config)
+
+        for keyhandler in ctx.keyhandler.values():
+            ctx.environ.update(keyhandler.env)
+
+    def _check_signatures(self, ctx):
+        for repo in ctx.config.get_repos():
+            if not repo.signed:
+                continue
+            valid, keyid = repo.check_signature()
+            keyhandler = ctx.keyhandler[repo.signers_type]
+            info = keyhandler.get_key_repr(keyid) if keyid else 'No info'
+            if valid:
+                logging.info(f'Repository {repo.name} signature valid: {info}')
+                continue
+            elif keyid:
+                raise RepoRefError(f'Repository {repo.name} is not signed '
+                                   f'with a trusted key: {info}')
+
+            raise RepoRefError(f'Repository {repo.name} is not signed '
+                               'with a trusted key.')

kas/repos.py
@@ -162,6 +162,37 @@ class Repo:
         return bool(output)
 
     @cached_property
+    def signers_type(self):
+        if self.allowed_signers is None:
+            return 'gpg'
+        signers = get_context().config.get_signers_config()
+        try:
+            ktypes = [signers[k].get('type', 'gpg')
+                      for k in self.allowed_signers]
+        except KeyError as e:
+            raise KasUserError(f'Repository {self.name}: '
+                               f'Allowed signer "{e}" not found in config')
+        if len(set(ktypes)) > 1:
+            raise KasUserError(f'Repository {self.name}: '
+                               'Mixed signer types are not supported')
+        return ktypes[0]
+
+    @property
+    def keyhandler(self):
+        if not self.signed:
+            return None
+        return get_context().keyhandler[self.signers_type]
+
+    def check_signature(self):
+        self.keyhandler.prepare_validation(self)
+        (ret, _, err) = run_cmd(self.is_signed_cmd(),
+                                cwd=self.path, fail=False, capture_stderr=True)
+        logging.debug('Signature verification output (%d):\n%s', ret, err)
+        if ret != 0:
+            return (False, None)
+        return self.keyhandler.validate_allowed_signer(self, err)
+
+    @cached_property
     def signed(self):
         return self.allowed_signers is not None
 
@@ -256,6 +287,9 @@ class Repo:
         path = repo_config.get('path', None)
         signed = repo_config.get('signed', False)
         signers = repo_config.get('allowed_signers', None) if signed else None
+        if signed and not signers:
+            raise KasUserError(f'Repository "{name}" is signed but no allowed '
+                               'signers specified.')
         disable_operations = False
 
         if path is None:
@@ -650,6 +684,13 @@ class GitRepo(RepoImpl):
     def is_dirty_cmd(self):
         return ['git', 'diff', '--stat']
 
+    def is_signed_cmd(self):
+        if self.tag:
+            return ['git', 'verify-tag', '--raw', self.tag]
+        else:
+            refspec = self.commit or self.revision
+            return ['git', 'verify-commit', '--raw', refspec]
+
     def resolve_branch_cmd(self):
         refspec = self.remove_ref_prefix(self.branch or self.refspec)
         return ['git', 'rev-parse', '--verify', '-q', f'origin/{refspec}']
@@ -730,6 +771,9 @@ class MercurialRepo(RepoImpl):
     def is_dirty_cmd(self):
         return ['hg', 'status', '--modified', '--added',
                 '--removed', '--deleted']
+
+    def is_signed_cmd(self):
+        raise NotImplementedError()
 
     def resolve_branch_cmd(self):
         if self.branch:

commit d6b9aa23a3fefc66c6a2dd6e5eaa532af054bdaa

kas: remove keyhandler dirs on purge
When running kas purge, all data manged by kas should be removed. By
that, we also need to remove the keyhandler dirs. We fix this by
registering them as managed directories.

Fixes: a2480fe59 ("add support to verify signatures of git repos")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/libcmds.py
@@ -622,6 +622,7 @@ class ReposCheckSignatures(Command):
         for _, d in handlerdirs.items():
             d.mkdir(exist_ok=True)
             d.chmod(0o700)
+            ctx.managed_paths.add(d)
 
         ctx.keyhandler['gpg'] = \
             GPGKeyHandler(handlerdirs['gpg'],