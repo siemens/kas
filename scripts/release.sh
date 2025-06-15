siemens/kas

commit 99d9604f0c5b52d89ec72a31c5be1e7161305a8f

kas-container: pass script version to kas in container
As a preparation to establish a stable interface between kas-container
and kas, we pass the kas-container script version as an environment
variable into the container. There it can be evaluated by kas.
We further terminate the container with a meaningful error message in
case a kas-container call is detected but kas inside the container does
not have a matching command.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas-container
@@ -27,7 +27,8 @@
 
 set -e
 
-KAS_IMAGE_VERSION_DEFAULT="4.7"
+KAS_CONTAINER_SCRIPT_VERSION="4.7"
+KAS_IMAGE_VERSION_DEFAULT="${KAS_CONTAINER_SCRIPT_VERSION}"
 KAS_CONTAINER_IMAGE_PATH_DEFAULT="ghcr.io/siemens/kas"
 KAS_CONTAINER_IMAGE_NAME_DEFAULT="kas"
 KAS_CONTAINER_SELF_NAME="$(basename "$0")"
@@ -584,6 +585,7 @@ fi
 set -- "$@" -v "${KAS_REPO_DIR}:/repo:${KAS_REPO_MOUNT_OPT}" \
 	-v "${KAS_WORK_DIR}":/work:rw -e KAS_WORK_DIR=/work \
 	--workdir=/repo \
+	-e KAS_CONTAINER_SCRIPT_VERSION="${KAS_CONTAINER_SCRIPT_VERSION}" \
 	-e USER_ID="$(id -u)" -e GROUP_ID="$(id -g)" --rm --init
 
 forward_dir KAS_BUILD_DIR "/build" "rw"

container-entrypoint
@@ -106,6 +106,10 @@ if [ -n "$1" ]; then
 		fi
 		;;
 	*)
+		if [ -n "$USER_ID" ]; then
+			echo "kas-container: this container does not support \"kas $1\"" >&2
+			exit 1
+		fi
 		# SC2086: Double quote to prevent globbing and word splitting.
 		# shellcheck disable=2086
 		exec $GOSU "$@"

commit c2a3faf6381bba639b40ee7523f5cab60e53f45d

kas-container: Update new version variable on release
This was forgotten when introducing KAS_CONTAINER_SCRIPT_VERSION,
causing the release of 4.8 to overwrite KAS_IMAGE_VERSION_DEFAULT again.
Fix up the variables and update the release script to use the right one
from now on.

Fixes: 99d9604f0c5b ("kas-container: pass script version to kas in container")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas-container
@@ -2,7 +2,7 @@
 #
 # kas - setup tool for bitbake based projects
 #
-# Copyright (c) Siemens AG, 2018-2024
+# Copyright (c) Siemens AG, 2018-2025
 #
 # Authors:
 #  Jan Kiszka <jan.kiszka@siemens.com>
@@ -27,8 +27,8 @@
 
 set -e
 
-KAS_CONTAINER_SCRIPT_VERSION="4.7"
-KAS_IMAGE_VERSION_DEFAULT="4.8"
+KAS_CONTAINER_SCRIPT_VERSION="4.8"
+KAS_IMAGE_VERSION_DEFAULT="${KAS_CONTAINER_SCRIPT_VERSION}"
 KAS_CONTAINER_IMAGE_PATH_DEFAULT="ghcr.io/siemens/kas"
 KAS_CONTAINER_IMAGE_NAME_DEFAULT="kas"
 KAS_CONTAINER_SELF_NAME="$(basename "$0")"

scripts/release.sh
@@ -31,7 +31,7 @@ fi
 
 mv newchangelog CHANGELOG.md
 sed -i "s,\(__version__ =\).*,\1 \'$NEW_VERSION\'," kas/__version__.py
-sed -i "s,\(KAS_IMAGE_VERSION_DEFAULT=\).*,\1\"$NEW_VERSION\"," kas-container
+sed -i "s,^\(KAS_CONTAINER_SCRIPT_VERSION=\).*,\1\"$NEW_VERSION\"," kas-container
 
 git add CHANGELOG.md
 git add kas/__version__.py