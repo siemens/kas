siemens/kas

commit fcbdb1617db4b77f4186ed93088008d7da63c05c

docs: extract default values from kas schema
By that we can get rid of the default values written as prosa.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

docs/_ext/sphinx_kas_schema.py
@@ -75,6 +75,7 @@ class KasSchemaDescRole(SphinxRole):
                 line=self.lineno,
             ))
             return [], messages
+        default = node.get('default', None)
 
         memo = Struct(document=self.inliner.document,
                       reporter=self.inliner.reporter,
@@ -83,6 +84,11 @@ class KasSchemaDescRole(SphinxRole):
         processed, msgs = self.inliner.parse(desc, self.lineno, memo, parent)
         parent += processed
         messages += msgs
+        if default:
+            def_pg = nodes.paragraph()
+            def_pg += nodes.strong(text='Default: ')
+            def_pg += nodes.literal(text=default)
+            parent += def_pg
 
         return parent, messages
 

commit 27bca9fba6de6d33f2c9b837caa4d6474b0fabd1

fix(spinx-schema): add defaults that evaluate to false as well
Our sphinx plugin that parses the jsonschema and extracts descriptions
from the nodes incorrectly handled default values that evalute to false
in Python (e.g. false, 0). We fix this by explicitly checking if the
value is available or not. By that, we get more defaults shown in the
documentation.

Fixes: fcbdb1617 ("docs: extract default values from kas schema")
Fixes: d558eb4bf ("docs: auto add enum values of schema node")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

docs/_ext/sphinx_kas_schema.py
@@ -85,18 +85,18 @@ class KasSchemaDescRole(SphinxRole):
         processed, msgs = self.inliner.parse(desc, self.lineno, memo, parent)
         parent += processed
         messages += msgs
-        if default:
+        if default is not None:
             def_pg = nodes.paragraph()
             def_pg += nodes.strong(text='Default: ')
-            def_pg += nodes.literal(text=default)
+            def_pg += nodes.literal(text=str(default))
             parent += def_pg
-        if allowed_values:
+        if allowed_values is not None:
             av_pg = nodes.paragraph()
             av_pg += nodes.strong(text='Supported values: ')
             for i in range(len(allowed_values)):
                 if i != 0:
                     av_pg += nodes.Text(', ')
-                av_pg += nodes.literal(text=allowed_values[i])
+                av_pg += nodes.literal(text=str(allowed_values[i]))
             parent += av_pg
 
         return parent, messages