siemens/kas

commit c59e2d5b169b6cd1fa6568c25b51eac766e87dd9

lock: print commit log on update
When updating the lockfile, we currently only print the old and the new
commit hashes. To make the output more meaningfull, we now also print
the commit log between both versions.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/plugins/lock.py
@@ -80,6 +80,7 @@ from kas.context import get_context
 from kas.includehandler import ConfigFile
 from kas.plugins.checkout import Checkout
 from kas.plugins.dump import Dump, IoTarget, LOCKFILE_VERSION_MIN
+from kas.plugins.diff import Diff
 from kas.repos import Repo
 
 __license__ = 'MIT'
@@ -105,6 +106,14 @@ class Lock(Checkout):
     def setup_parser(cls, parser):
         super().setup_parser(parser)
         Dump.setup_parser_format_args(parser)
+
+    def _print_log_diff(self, repo, old_commit):
+        try:
+            diff = repo.diff(old_commit, None)
+        except NotImplementedError:
+            return
+        Diff.formatting_diff_output(
+            None, None, {'vcs': diff}, True, False, True, False)
 
     def _update_lockfile(self, lockfile, repos_to_lock, update_only, args):
         """
@@ -138,6 +147,7 @@ class Lock(Checkout):
             elif not lockfile.is_external:
                 logging.info('Updating lock of %s: %s -> %s',
                              r.name, v['commit'], r.revision)
+                self._print_log_diff(r, v['commit'])
                 v['commit'] = r.revision
                 changed = True
             else:

commit 2431fb476da6d0bdc598842471cbfcab5940cdde

fix: catch error when updating an invalid lockfile
Currently, kas doesn't validate if a lockfile commit is valid when
calling 'kas lock --update'. This command would just replace it with a
new valid commit, but generating the diff between the two commits always
raises an uncaught error. This change logs the problem and then proceeds
with the replacement as intended.

Fixes: c59e2d5b1 ("lock: print commit log on update")
Signed-off-by: Tamino Larisch <tamino.larisch@siemens.com>
[Jan: wrapped overlong line]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/plugins/lock.py
@@ -81,7 +81,7 @@ from kas.includehandler import ConfigFile
 from kas.plugins.checkout import Checkout
 from kas.plugins.dump import Dump, IoTarget, LOCKFILE_VERSION_MIN
 from kas.plugins.diff import Diff
-from kas.repos import Repo
+from kas.repos import Repo, RepoRefError
 
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) Siemens AG, 2024'
@@ -111,6 +111,9 @@ class Lock(Checkout):
         try:
             diff = repo.diff(old_commit, None)
         except NotImplementedError:
+            return
+        except RepoRefError as e:
+            logging.warning(e)
             return
         Diff.formatting_diff_output(
             None, None, {'vcs': diff}, True, False, True, False)

kas/repos.py
@@ -39,7 +39,7 @@ from .context import get_context
 from .libkas import run_cmd_async, run_cmd
 from .kasusererror import KasUserError
 from functools import cached_property
-from git import Repo as GitPythonRepo
+from git import GitCommandError, Repo as GitPythonRepo
 
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) Siemens AG, 2017-2018'
@@ -805,8 +805,23 @@ class GitRepo(RepoImpl):
         shallow_file = os.path.join(git_repo.git_dir, 'shallow')
         if os.path.isfile(shallow_file):
             git_repo.git.fetch(unshallow=True)
-        commits = list(git_repo.iter_commits(
-                       f'{commit1}..{commit2}'))
+        try:
+            commits = list(git_repo.iter_commits(
+                           f'{commit1}..{commit2}'))
+        except GitCommandError as e:
+            valid1 = git_repo.is_valid_object(commit1)
+            valid2 = git_repo.is_valid_object(commit2)
+            if not valid1 and not valid2:
+                error_msg = f'Commits ({commit1} and {commit2}) are not valid'
+            elif not valid1:
+                error_msg = f'First commit ({commit1}) is not valid'
+            elif not valid2:
+                error_msg = f'Second commit ({commit2}) is not valid'
+            else:
+                error_msg = str(e)
+            raise RepoRefError(
+                'Could not compute diff for repository '
+                f'"{self.name}": {error_msg}')
         diff_json = {self.name: []}
         for commit in commits:
             diff_json[self.name].append({