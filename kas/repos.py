siemens/kas

commit 397b10d5fab5f7c72b4dfa2f11a27a5cfad06535

repos: Warn about repos with branches but without commit or lock file
So far, we assumed that a branch-only repo would be accompanied by a
lock file to pin its commit. But users may forget the latter step or
even do not know about it.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/repos.py
@@ -239,7 +239,7 @@ class Repo:
                f'{self.path} {self.layers}'
 
     __legacy_refspec_warned__ = []
-    __no_commit_tag_warned__ = []
+    __no_commit_warned__ = []
 
     @staticmethod
     def factory(name, repo_config, repo_defaults, repo_fallback_path,
@@ -296,11 +296,16 @@ class Repo:
                     'Unsupported mixture of legacy refspec and '
                     f'commit/tag/branch for repository "{name}"')
             refspec = repo_overrides.get('commit', refspec)
-        if tag and not commit:
-            if name not in Repo.__no_commit_tag_warned__:
+        if not commit and name not in Repo.__no_commit_warned__:
+            if tag:
                 logging.warning('Using tag without commit for repository '
                                 '"%s" is unsafe as tags are mutable.', name)
-                Repo.__no_commit_tag_warned__.append(name)
+                Repo.__no_commit_warned__.append(name)
+            elif branch:
+                logging.warning('Using branch without commit for repository '
+                                '"%s" is unsafe. Either add a commit or use '
+                                'a lock file.', name)
+                Repo.__no_commit_warned__.append(name)
         path = repo_config.get('path', None)
         signed = repo_config.get('signed', False)
         signers = repo_config.get('allowed_signers', None) if signed else None

tests/test_refspec.py
@@ -175,7 +175,7 @@ def test_unsafe_tag_warning(capsys, monkeykas, tmpdir):
     shutil.copytree('tests/test_refspec', tdir)
     monkeykas.chdir(tdir)
     # needs to be reset in case other tests ran before
-    Repo.__no_commit_tag_warned__ = []
+    Repo.__no_commit_warned__ = []
     kas.kas(['shell', 'test2.yml', '-c', 'true'])
     assert capsys.readouterr().err.count(
         'Using tag without commit for repository "kas4" is unsafe as tags '

commit 92fc42563ff8fff22a2be22badc28492e0cbfdcd

repos: Do not warn about unpinned local repos
Repos without URL are local ones and a are not version-controlled from
kas perspective. Do not warn if they appear to be unpinned, e.g. due to
getting a branch via a default assignment.

Reported-by: Frieder Schrempf <frieder.schrempf@kontron.de>
Fixes: 397b10d5fab5 ("repos: Warn about repos with branches but without commit or lock file")
Tested-by: Frieder Schrempf <frieder.schrempf@kontron.de>
Reviewed-by: Frieder Schrempf <frieder.schrempf@kontron.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/repos.py
@@ -298,7 +298,7 @@ class Repo:
                     'Unsupported mixture of legacy refspec and '
                     f'commit/tag/branch for repository "{name}"')
             refspec = repo_overrides.get('commit', refspec)
-        if not commit and name not in Repo.__no_commit_warned__ \
+        if url and not commit and name not in Repo.__no_commit_warned__ \
                 and get_context().unpinned_repo_warnings:
             if tag:
                 logging.warning('Using tag without commit for repository '